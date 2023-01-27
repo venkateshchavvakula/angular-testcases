b2f1729921c1e44ded3bda49eb2f489c5d6a0787
venki0004 <venkatesh@scube.me>
2023-01-27 22:33:47 +0530

    updste test cases

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -5,13 +5,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { AuthService } from '../core/services/auth.service';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let authService:AuthService
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports:[ MatInputModule,
@@ -26,6 +28,9 @@ describe('LoginComponent', () => {
       declarations: [ LoginComponent ]
     })
     .compileComponents();
+
+    authService = TestBed.inject(AuthService);
+    spyOn(authService, 'login').and.returnValue(of('test'))
   });
 
   beforeEach(() => {
@@ -37,4 +42,5 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
